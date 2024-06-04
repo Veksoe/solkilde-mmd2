af1f72d4d43867825d13536a1b7b8576f2db3698
Veksoe <Anniika-xb@live.dk>
2024-06-04 18:31:23 +0200

    Index mobil update, produkt oversiter done (umiddelbart)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@ const baseUrl = "https://solkilde-api.annikavekso.com/wp-json/wp/v2/posts"
 
 const produktListeContainerEl = document.querySelector(".produktContainer")
 const produktListeIndexContainerEl = document.querySelector(".produktContainer.index")
+const produktListeMoblerContainerEl = document.querySelector(".produktContainer.mobler")
+const produktListeBoligdekorationContainerEl = document.querySelector(".produktContainer.boligdekoration")
 
 const filterBtnEl = document.querySelector(".filterBtn")
 const filterContainernEl = document.querySelector(".filterContainer")
@@ -13,7 +15,20 @@ const footerEl = document.querySelector("footer")
 KALD AF FUNKTIONERNE
 *************************************/
 if (produktListeIndexContainerEl) {
-    hentProdukter(produktListeContainerEl, 4)
+    hentProdukter(produktListeContainerEl, "34,37", 4)
+}
+if (produktListeMoblerContainerEl) {
+    hentProdukter(produktListeMoblerContainerEl, "34", 9)
+}
+if (produktListeBoligdekorationContainerEl) {
+    hentProdukter(produktListeBoligdekorationContainerEl, "37", 9)
+}
+
+if (footerEl) {
+    renderFooter();
+}
+if (navEl) {
+    renderNav();
 }
 
 /************************************
@@ -21,8 +36,8 @@ HENT PRODUKTER PÅ FORSKELLIGE MÅDER
 *************************************/
 
 /* Hent alle produkter og tag en placering ind */
-function hentProdukter(placering, antal) {
-    fetch(baseUrl + "?per_page=" + antal)
+function hentProdukter(placering, kategori, antal) {
+    fetch(baseUrl + "?categories=" + kategori + "&per_page=" + antal)
         .then(res => res.json())
         .then(data => {
             data.forEach(produkt => renderPreviewProdukt(produkt, placering)) /* For hvert object i datet, kør funktionen til at rendere preview af en produkt/produkt card. */
@@ -54,4 +69,42 @@ function renderPreviewProdukt(produkt, placering) {
                 </article>
 
             </div>`
+}
+
+function renderNav() {
+    navEl.innerHTML += `
+    <p class="logo">Solkilde</p>
+    <button aria-label="menu"><i class="fa-solid fa-bars"></i></button>
+    <!--https://fontawesome.com/icons/bars?f=classic&s=solid-->
+`
+}
+
+function renderFooter() {
+    footerEl.innerHTML += `
+    <p class="logo">Solkilde</p>
+    <p>Åbent hver weekend og <br> helligdage fra 12-16 og efter aftale.</p>
+    <div class="telefon">
+        <i class="fa-solid fa-phone"></i><a href="tel:+4523951990">+45 2395
+            1990</a><!--https://fontawesome.com/icons/phone?f=classic&s=solid-->
+    </div>
+    <div>
+        <i class="fa-brands fa-instagram"></i> <a href="">solkilde.tversted</a>
+        <!--https://fontawesome.com/icons/instagram?f=brands&s=solid-->
+    </div>
+    <div>
+        <i class="fa-regular fa-envelope"></i> <a
+            href="mailto:solkilde.tversted@gmail.com ">solkilde.tversted@gmail.com </a>
+        <!--https://fontawesome.com/icons/envelope?f=classic&s=regular-->
+    </div>
+    <div>
+        <i class="fa-solid fa-store"></i>
+        <address>Tannisbugtvej 103 <br>
+            9881 Bindslev<br>
+            Danmark</address> <!--https://fontawesome.com/icons/store?f=classic&s=solid-->
+    </div>
+    <div class="copyright">
+        <i class="fa-regular fa-copyright"></i>
+        <p>Solkilde</p> <!--https://fontawesome.com/icons/copyright?f=classic&s=regular-->
+    </div>
+`
 }
